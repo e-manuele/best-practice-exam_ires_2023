6caa061949b4e406670f24aeed6926c4886d7b79
Dioico <74198922+Dioico@users.noreply.github.com>
2023-08-03 12:13:48 +0200

    clean OldFashionPoundApp

--- src/OldFashionPoundApp.java
+++ src/OldFashionPoundApp.java
@@ -50,41 +50,6 @@ FARE PER OGNI OP UN OGGETTO(?)
 
 
 
-
-
-        /*
-        System.out.print("Example SUM:\n" +
-                "5p 17s 8d + 3p 4s 10d = 9p 2s 6d \n" +
-                "op.sum(p1,p2) -> ");
-        System.out.println(op.sum(p1, p2));
-        System.out.println();
-        System.out.print("Example SUBTRACTION:\n" +
-                "5p 17s 8d - 3p 4s 10d = 2p 12s 10d \n" +
-                "op.subtraction(p1,p2) -> ");
-        System.out.println();
-        System.out.println(op.subtraction(p1, p2));
-        System.out.print("Subtractions giving negative results are managed:\n" +
-                "5p 17s 8d - 5p 18s 8d = 0d (-1s 0d) \n" +
-                "op.subtraction(p1,p3) -> ");
-        System.out.println(op.subtraction(p1, p3));
-        System.out.println();
-
-        System.out.print("Example MULTIPLICATION:\n" +
-                "5p 17s 8d * 2 = 11p 15s 4d \n" +
-                "op.subtraction(p1,p3) -> ");
-        System.out.println(op.multiplication(p1, 2));
-        System.out.println();
-        System.out.print("Example DIVISION:\n" +
-                "5p 17s 8d / 3 = 1p 19s 2d (2p)\n" +
-                "op.division(p1,3) -> ");
-        System.out.println(op.division(p1, 3));
-        System.out.println();
-        System.out.print("Another Example DIVISION\n" +
-                "18p 16s 1d / 15 = 1p 5s 0d (1s 1d) \n" +
-                "op.division(p4,15) -> ");
-        System.out.println(op.division(p4, 15));
-        System.out.println();
-*/
     }
 
 
